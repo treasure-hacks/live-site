b6b003fd082f009a2c57e8f246b96d134432af38
Anthony <55033931+anthonyj33@users.noreply.github.com>
2023-09-06 00:15:30 -0700

    Use guard clauses

--- src/main.js
+++ src/main.js
@@ -2,26 +2,22 @@
 const SUBSCRIBE_ENDPOINT = 'https://api.treasurehacks.org/notifications/live-site/subscribe';
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register(new URL('/src/service-worker.js', import.meta.url)).then(sw => {
-    if ('Notification' in window) {
-      if (Notification.permission !== 'granted') {
-        document.body.addEventListener('click', () => Notification.requestPermission().then(permission => {
-          if (permission === 'granted') {
-            sw.pushManager.subscribe({
-              userVisibleOnly: true,
-              // Server's public key
-              applicationServerKey: 'BD_DXIXKbM6eMNm6SejSYnaZbVaM_ekjc_WAXBEPa4pUpf9E7kXEpV-8KtzgSrcl_rk-_yGbonNwyLLe69aJGvE'
-            }).then(sub => {
-              window.sub = sub;
-              const subscriptionBody = JSON.stringify({
-                subscription: sub.toJSON(),
-                groups: ['announcements']
-              });
-              fetch(SUBSCRIBE_ENDPOINT, { headers: { 'Content-Type': 'application/json' }, body: subscriptionBody, method: 'POST' });
-            });
-          }
-        }), { once: true });
-      }
-    }
+    if (!('Notification' in window) || Notification.permission === 'granted') return;
+    document.body.addEventListener('click', () => Notification.requestPermission().then(permission => {
+      if (permission !== 'granted') return;
+      sw.pushManager.subscribe({
+        userVisibleOnly: true,
+        // Server's public key
+        applicationServerKey: 'BD_DXIXKbM6eMNm6SejSYnaZbVaM_ekjc_WAXBEPa4pUpf9E7kXEpV-8KtzgSrcl_rk-_yGbonNwyLLe69aJGvE'
+      }).then(sub => {
+        window.sub = sub;
+        const subscriptionBody = JSON.stringify({
+          subscription: sub.toJSON(),
+          groups: ['announcements']
+        });
+        fetch(SUBSCRIBE_ENDPOINT, { headers: { 'Content-Type': 'application/json' }, body: subscriptionBody, method: 'POST' });
+      });
+    }), { once: true });
   });
 }
 
